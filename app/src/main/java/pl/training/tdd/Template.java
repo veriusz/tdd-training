642e33b332d12a4cb28d9c6cea3eef71e27e511f
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2020-11-30 13:57:09 +0100

    004 evaluation_of_template_with_simple_text_returns_the_text (red)

--- app/src/main/java/pl/training/tdd/Template.java
+++ app/src/main/java/pl/training/tdd/Template.java
@@ -2,6 +2,9 @@ package pl.training.tdd;
 
 public class Template {
 
+    public Template(String testWithExpressions) {
+    }
+
     public String evaluate() {
         return "";
     }
--- app/src/test/java/pl/training/tdd/TemplateTest.java
+++ app/src/test/java/pl/training/tdd/TemplateTest.java
@@ -5,13 +5,20 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ApplicationTest {
+public class TemplateTest {
 
     @DisplayName("Given an empty template, when evaluating, then should return an empty text")
     @Test
     void evaluation_of_empty_template_returns_an_empty_string() {
-        var template = new Template();
+        var template = new Template("");
         assertEquals("", template.evaluate());
+    }
+
+    @DisplayName("Given an template with simple text (without expressions), when evaluating, then should return the text")
+    @Test
+    void evaluation_of_template_with_simple_text_returns_an_text_string() {
+        var template = new Template("My name is Jan");
+        assertEquals("My name is Jan", template.evaluate());
     }
 
 }
