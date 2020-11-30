427057a7dc38f4ab3cdb1a52294ef250de4080a6
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2020-11-30 13:59:05 +0100

    005 evaluation_of_template_with_simple_text_returns_the_text (green)

--- app/src/main/java/pl/training/tdd/Template.java
+++ app/src/main/java/pl/training/tdd/Template.java
@@ -2,11 +2,14 @@ package pl.training.tdd;
 
 public class Template {
 
+    private final String textWithExpressions;
+
     public Template(String testWithExpressions) {
+        this.textWithExpressions = testWithExpressions;
     }
 
     public String evaluate() {
-        return "";
+        return textWithExpressions;
     }
 
 }
