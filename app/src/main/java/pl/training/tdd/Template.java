5b3c6914ef1754f17dcef38c803dc3db52af64f0
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2020-11-30 14:07:50 +0100

    007 evaluation_of_template_without_providing_all_values_throw_an_exception (red)

--- app/src/main/java/pl/training/tdd/Template.java
+++ app/src/main/java/pl/training/tdd/Template.java
@@ -1,5 +1,7 @@
 package pl.training.tdd;
 
+import java.util.Map;
+
 public class Template {
 
     private final String textWithExpressions;
@@ -8,7 +10,7 @@ public class Template {
         this.textWithExpressions = testWithExpressions;
     }
 
-    public String evaluate() {
+    public String evaluate(Map<Object, Object> valuesMap) {
         return textWithExpressions;
     }
 
--- app/src/test/java/pl/training/tdd/TemplateTest.java
+++ app/src/test/java/pl/training/tdd/TemplateTest.java
@@ -3,7 +3,9 @@ package pl.training.tdd;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static java.util.Collections.emptyMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TemplateTest {
 
@@ -11,14 +13,22 @@ public class TemplateTest {
     @Test
     void evaluation_of_empty_template_returns_an_empty_string() {
         var template = new Template("");
-        assertEquals("", template.evaluate());
+        assertEquals("", template.evaluate(emptyMap()));
     }
 
     @DisplayName("Given an template with simple text (without expressions), when evaluating, then should return the text")
     @Test
     void evaluation_of_template_with_simple_text_returns_an_text_string() {
         var template = new Template("My name is Jan");
-        assertEquals("My name is Jan", template.evaluate());
+        assertEquals("My name is Jan", template.evaluate(emptyMap()));
+    }
+
+    @DisplayName("Given an template, when evaluating without providing all values, then should throw an exception")
+    @Test
+    void evaluation_of_template_without_providing_all_values_throw_an_exception() {
+        var template = new Template("My name is ${firstName}");
+        var valuesMap = emptyMap();
+        assertThrows(IllegalArgumentException.class, () -> template.evaluate(valuesMap));
     }
 
 }
