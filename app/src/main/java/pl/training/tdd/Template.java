679c0f74cb7b1a4eb29d50cc79fe0509f6ae28e7
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2020-11-30 14:45:55 +0100

    015 evaluation_of_template_returns_text_with_substituted_values (refactor)

--- app/src/main/java/pl/training/tdd/Template.java
+++ app/src/main/java/pl/training/tdd/Template.java
@@ -1,12 +1,15 @@
 package pl.training.tdd;
 
 import java.util.Map;
+import java.util.function.BinaryOperator;
 import java.util.regex.Pattern;
 
 public class Template {
 
     private static final Pattern EXPRESSION = Pattern.compile("\\$\\{\\w+}");
     private static final String INVALID_VALUE = "\\W+";
+    private static final String EXPRESSION_START = "\\$\\{";
+    private static final String EXPRESSION_END = "}";
 
     private final String textWithExpressions;
 
@@ -18,8 +21,7 @@ public class Template {
         if (containsInvalidValues(valuesMap) || isComplete(valuesMap)) {
             throw new IllegalArgumentException();
         }
-        return valuesMap.keySet().stream()
-                .reduce(textWithExpressions, (result, key) -> result.replaceAll(createExpression(key), valuesMap.get(key)));
+        return valuesMap.keySet().stream().reduce(textWithExpressions, substitute(valuesMap));
     }
 
     private boolean isComplete(Map<String, String> valuesMap) {
@@ -34,8 +36,12 @@ public class Template {
         return valuesMap.values().stream().anyMatch(value -> value.matches(INVALID_VALUE));
     }
 
+    private BinaryOperator<String> substitute(Map<String, String> valuesMap) {
+        return (result, key) -> result.replaceAll(createExpression(key), valuesMap.get(key));
+    }
+
     private String createExpression(String key) {
-        return "\\$\\{" + key + "}";
+        return EXPRESSION_START + key + EXPRESSION_END;
     }
 
 }
