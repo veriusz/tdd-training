50b83adc881f5efe02ee33083c858340c05d47a3
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2020-11-30 14:26:16 +0100

    011 evaluation_of_template_with_non_alphanumeric_values_throw_an_exception (green)

--- app/src/main/java/pl/training/tdd/Template.java
+++ app/src/main/java/pl/training/tdd/Template.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 public class Template {
 
     public static final Pattern EXPRESSION = Pattern.compile("\\$\\{\\w+}");
+    private static final String INVALID_VALUE = "\\W+";
 
     private final String textWithExpressions;
 
@@ -15,6 +16,9 @@ public class Template {
 
     public String evaluate(Map<String, String> valuesMap) {
         if (getExpressionsCount() !=  valuesMap.size()) {
+            throw new IllegalArgumentException();
+        }
+        if (valuesMap.values().stream().anyMatch(value -> value.matches(INVALID_VALUE))) {
             throw new IllegalArgumentException();
         }
         return textWithExpressions;
