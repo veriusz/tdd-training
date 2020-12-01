e2b58c59a286b6304b60db83f9b522dab34ec6de
Szymon <szymonjakubiak@live.com>
2020-12-01 12:44:41 +0100

    Money 002 (green)

--- app/src/test/java/pl/training/tdd/MoneyTest.java
+++ app/src/test/java/pl/training/tdd/MoneyTest.java
@@ -3,12 +3,14 @@ package pl.training.tdd;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 public class MoneyTest {
 
     @Test
     void should_return_amount_zero() {
         var money = new Money();
-        Assertions.assertEquals(0.0, money.getAmount());
+        Assertions.assertEquals(BigDecimal.ZERO, money.getAmount());
     }
 
 }
